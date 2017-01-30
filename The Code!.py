91ecbd68f52d7865fc93a1f8a4bc204421f07e4b
ndspikmin <ndstotlar@icloud.com>
2017-01-30 13:33:23 -0500

    Changed some language...

--- The Code!.py
+++ The Code!.py
@@ -117,6 +117,6 @@ def start():
         time.sleep(1)
         print("Game Over. 'Restart code to retry'")
     else:
-        print("YOURE A LOUSY IDIOT WHO DOESNT DESERVE TO PLAY THIS AMAZING GAME!!! GET THE F*** OUT!")
+        print("YOURE A LOUSY IDIOT WHO DOESNT DESERVE TO PLAY THIS AMAZING GAME!!!")
 start()
 
