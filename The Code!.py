8cc1d2ecb28e82886a9ffecc4f4103d5bafd37b3
ealgase <mostdigitsofpi@gmail.com>
2017-01-30 12:49:08 -0500

    Update The Code!.py

--- The Code!.py
+++ The Code!.py
@@ -9,15 +9,15 @@ def start():
         Pizza = input("yes/no: ")
     if Pizza == "yes":
         print("I guess you arent so bad after all...")
-        time.sleep(5)
+        time.sleep(1)
     else:
         print("booo")
-        time.sleep(5)
+        time.sleep(1)
     bye()
 
 def bye():
     print("I guess I'll send you on your way then old friend.")
-    time.sleep(4)
+    time.sleep(1)
 
     path()
 
@@ -31,12 +31,12 @@ def path():
 
 def camp():
     print("You set up camp but can't get a fire started.")
-    time.sleep(5)
+    time.sleep(1)
     run()
 
 def walk():
     print("The village is farther away than you think but you keep walking.")
-    time.sleep(5)
+    time.sleep(1)
     fight()
 
 def fight():
@@ -55,16 +55,16 @@ def run():
         
 def bad():
     print("You manage to kill one zombie but the rest swarm you")
-    time.sleep(2)
+    time.sleep(1)
     print("Game Over. 'Restart code to retry'")
           
 def flee():
     print("You manage to get away and make it to the village.")
-    time.sleep(3)
+    time.sleep(1)
     print("You find a nice person and they let you have some food and time.sleep for the night.")
-    time.sleep(5)
+    time.sleep(1)
     print("When you wake up the village is abandoned. You wonder whats going on and run outside.")
-    time.sleep(3)
+    time.sleep(1)
     go = input("You start to enter a cave but don't know if it is a good idea. Go down 'go' or Don't go down 'dont'")
     if go == "go":
         down()
@@ -73,12 +73,12 @@ def flee():
 
 def dont():
     print("As you are leaving a creeper comes behind you and blows you up.")
-    time.sleep(2)
+    time.sleep(1)
     print("Game Over. 'Restart code to retry'")
         
 def down():
     print("You find an empty chest and decide to take it. Could be helpful later.")
-    time.sleep(3)
+    time.sleep(1)
     cave = input("You suddenly are attacked by behind. Fight 'fight' or Run 'run'")
     if cave == "fight":
         lucky()
@@ -87,16 +87,16 @@ def down():
 
 def ded():
     print("You try to blend into the cave but the spider has good eye sight and spots you.")
-    time.sleep(3)
+    time.sleep(1)
     print("Game Over. 'Restart code to retry'")
 
 def lucky():
     print("You slay the spider and grab the string he drops.")
-    time.sleep(4)
+    time.sleep(1)
     print("You here a voice in the distance, it sounds like someone you know...")
-    time.sleep(4)
+    time.sleep(1)
     print("Cody comes around the corner and sees you.")
-    time.sleep(3)
+    time.sleep(1)
     print("Oh hello! I thought I might find you here")
     
     if Pizza == "yes":
@@ -106,22 +106,22 @@ def lucky():
 
 def Lose():
     print("Remember the Pizza question in the beginning? Yeah that kinda decides if you win or not, sorry f or not telling you!")
-    time.sleep(4)
+    time.sleep(1)
     print("But we're still friends, right?")
-    time.sleep(3)
+    time.sleep(1)
     print("Well then I will see you in the end old friend!")
-    time.sleep(4)
+    time.sleep(1)
     print("Game Over. 'Restart code to retry'")
 
 def Win():
     print("Remember the Pizza question in the beginning? Yeah that kinda decides if you win or not, sorry for not telling you!")
-    time.sleep(4)
+    time.sleep(1)
     print("But because you entered yes you WIN!... a free back massage!")
-    time.sleep(2)
+    time.sleep(1)
     print("Yeah... That kinda sounded better in my head...")
-    time.sleep(3)
+    time.sleep(1)
     print("Well I will see you in the next game!")
-    time.sleep(3)
+    time.sleep(1)
     print("You WIN! Play my next game when it comes out!")
     
 start()
